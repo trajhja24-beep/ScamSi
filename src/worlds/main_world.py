0ae9250c19b5fddb690fc17052e27a6caa97e3e1
trajhja24-beep <trajhja24@sps-prosek.cz>
2026-02-25 18:43:25 +0100

    i guess work kind of

--- src/worlds/main_world.py
+++ src/worlds/main_world.py
@@ -1,11 +1,16 @@
 from worlds.object_create import WorldObject
 from worlds.minigame import MiniGame
+from worlds.desktop import Screen
 from settings import *
 from global_set.ui import RenderText
+
 import pygame
+import settings
+import math
 
 class MainWorld:
     def __init__(self, game):
+        self.enemy_count = 2
         self.game = game
         self.camera = [0, 0]
         self.hintText = RenderText(100, HEIGHT - 100, GREEN, "Press E")
@@ -27,7 +32,8 @@ class MainWorld:
         if keys[pygame.K_e]:
             for object in self.objects:
                 if object.is_colliding(self.game.player, self.camera):
-                    self.game.current_state = MiniGame(self.game, screen)
+                    
+                    self.game.current_state = Screen(self.game, screen) #MiniGame(self.game, screen, self.enemy_count)
     def draw(self, screen):
         screen.fill(BLACK)
         self.background.draw(screen, self.camera)
@@ -35,6 +41,7 @@ class MainWorld:
             object.draw(screen, self.camera)
         self.game.player.draw(screen)
         self.game.progress_bar.draw(screen)
+        RenderText(WIDTH - 200, HEIGHT - 100, GREEN, "$" + str(settings.MONEY)).draw(screen)
         for object in self.objects:
             if object.is_colliding(self.game.player, self.camera):
                 self.hintText.draw(screen)
--- src/worlds/minigame.py
+++ src/worlds/minigame.py
@@ -1,52 +1,66 @@
 import pygame
+import settings
 from settings import *
 from entities.entities import Enemy
 
 class MiniGame:
-    def __init__(self, game, screen):
+    def __init__(self, game, screen, enemy_count):
+        self.minigame_start = False
         self.game = game
         self.main_screen = screen
-        self.player = pygame.Rect((WIDTH - WIDTH / 2) / 2 + (WIDTH / 2) / 20, (HEIGHT - HEIGHT / 2) / 2 + HEIGHT / 2 - ((WIDTH / 2) / 20 + 10), (WIDTH / 2) / 20, (WIDTH / 2) / 20)
+        self.player = pygame.Rect((settings.WIDTH - WIDTH / 2) / 2 + (WIDTH / 2) / 20, (HEIGHT - HEIGHT / 2) / 2 + HEIGHT / 2 - ((WIDTH / 2) / 20 + 10), (WIDTH / 2) / 20, (WIDTH / 2) / 20)
         self.finish = pygame.Rect((WIDTH - WIDTH / 2) / 2 + WIDTH / 2 - ((WIDTH / 2) / 20 + 10), (HEIGHT - HEIGHT / 2) / 2 + (WIDTH / 2) / 20, (WIDTH / 2) / 20, (WIDTH / 2) / 20)
         self.rect = pygame.Rect(((WIDTH - WIDTH / 2) / 2 ,(HEIGHT - HEIGHT / 2) / 2), (WIDTH / 2, HEIGHT / 2)) 
-        
-        self.enemy = Enemy(pygame.Rect((WIDTH / 4), (HEIGHT / 4), (WIDTH / 2) / 20, (WIDTH / 2) / 20), self.main_screen)
+        self.enemys = []
+        for x in range(enemy_count):
+            self.enemys.append(Enemy(pygame.Rect((WIDTH / 2), (HEIGHT / 2), (WIDTH / 2) / 20, (WIDTH / 2) / 20), self.main_screen))
 
     def update(self, dt, screen):
         keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_a] or keys[pygame.K_d] or keys[pygame.K_s] or keys[pygame.K_w]:
+            self.minigame_start = True
 
-        if keys[pygame.K_a]:
-            if self.player.left - MINIGAME_PLAYER_SPEED >= self.rect.left:
-                self.player.x -= MINIGAME_PLAYER_SPEED
 
-        if keys[pygame.K_d]:
-            if self.player.right + MINIGAME_PLAYER_SPEED <= self.rect.right:
-                self.player.x += MINIGAME_PLAYER_SPEED
+        if self.minigame_start:
 
-        if keys[pygame.K_w]:
-            if self.player.top - MINIGAME_PLAYER_SPEED >= self.rect.top:
-                self.player.y -= MINIGAME_PLAYER_SPEED
+            if keys[pygame.K_a]:
+                if self.player.left - MINIGAME_PLAYER_SPEED >= self.rect.left:
+                    self.player.x -= MINIGAME_PLAYER_SPEED
 
-        if keys[pygame.K_s]:
-            if self.player.bottom + MINIGAME_PLAYER_SPEED <= self.rect.bottom:
-                self.player.y += MINIGAME_PLAYER_SPEED
+            if keys[pygame.K_d]:
+                if self.player.right + MINIGAME_PLAYER_SPEED <= self.rect.right:
+                    self.player.x += MINIGAME_PLAYER_SPEED
 
-        self.enemy.move_toward(self.player)
+            if keys[pygame.K_w]:
+                if self.player.top - MINIGAME_PLAYER_SPEED >= self.rect.top:
+                    self.player.y -= MINIGAME_PLAYER_SPEED
 
-        if self.player.colliderect(self.finish):
-            self.game.progress_bar.set(
-                self.game.progress_bar.progress - 10
-            )
-            self.game.current_state = self.game.main_world
+            if keys[pygame.K_s]:
+                if self.player.bottom + MINIGAME_PLAYER_SPEED <= self.rect.bottom:
+                    self.player.y += MINIGAME_PLAYER_SPEED
+            for enemy in self.enemys:
+                enemy.move_toward(self.player)
 
-        # if self.player.colliderect(self.enemy.rect):
-        #     self.game.progress_bar.set(
-        #         self.game.progress_bar.progress + 10
-        #     )
-        #     self.game.current_state = self.game.main_world
+            if self.player.colliderect(self.finish):
+                self.game.progress_bar.set(
+                    self.game.progress_bar.progress - 5
+                )
+                settings.MONEY += 10
+                self.game.current_state = self.game.main_world
+            for enemy in self.enemys:
+                if self.player.colliderect(enemy.enemy):
+                    self.game.progress_bar.set(
+                        self.game.progress_bar.progress + 10
+                    )
+
+                    settings.MONEY -= 5
+
+                    self.game.current_state = self.game.main_world
 
     def draw(self, main_screen):
         pygame.draw.rect(self.main_screen, (10, 10, 10), self.rect)
         pygame.draw.rect(self.main_screen, GREEN, self.finish)
         pygame.draw.rect(self.main_screen, RED, self.player)
-        self.enemy.draw(self.main_screen)
+        for enemy in self.enemys:
+            enemy.draw(self.main_screen)
