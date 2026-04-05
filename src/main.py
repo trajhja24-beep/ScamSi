300a3aa9f1463e7cf7d70d19dc3a7a06c61c6a5f
trajhja24-beep <trajhja24@sps-prosek.cz>
2026-04-05 07:56:54 +0200

    toml

--- src/main.py
+++ src/main.py
@@ -9,43 +9,46 @@ from global_set.ui import Inventory
 from worlds.main_world import MainWorld
 
 
+def main():
+    pygame.init()
+    pygame.mixer.init()
 
-pygame.init()
-pygame.mixer.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-player_path = os.path.join(BASE_DIR, "assets", "player.png")
-player_img = pygame.image.load(player_path).convert_alpha()
-player_img = pygame.transform.scale(player_img, PLAYER_SIZE)
+    player_path = os.path.join(BASE_DIR, "assets", "player.png")
+    player_img = pygame.image.load(player_path).convert_alpha()
+    player_img = pygame.transform.scale(player_img, PLAYER_SIZE)
 
 
-progress_bar = ProgressBar(WIDTH - 250, HEIGHT - 40, 200, 20)
+    progress_bar = ProgressBar(WIDTH - 250, HEIGHT - 40, 200, 20)
 
-class Game:
-    def __init__(self):
-        self.progress_bar = progress_bar
-        self.inventory =Inventory(SLOTCOUNT, 0, 0, self)
-        self.main_world = MainWorld(self)
-        self.player = Player(player_img, PLAYER_STARTING_POSITION, self.main_world)
-        self.current_state = self.main_world
+    class Game:
+        def __init__(self):
+            self.progress_bar = progress_bar
+            self.inventory =Inventory(SLOTCOUNT, 0, 0, self)
+            self.main_world = MainWorld(self)
+            self.player = Player(player_img, PLAYER_STARTING_POSITION, self.main_world)
+            self.current_state = self.main_world
 
-game = Game()
+    game = Game()
 
-running = True
-while running:
-    dt = clock.tick(FPS) / 1000
+    running = True
+    while running:
+        dt = clock.tick(FPS) / 1000
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    game.current_state.update(dt, screen)
-    game.current_state.draw(screen)
+        game.current_state.update(dt, screen)
+        game.current_state.draw(screen)
 
-    pygame.display.flip()
+        pygame.display.flip()
 
-pygame.quit()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
